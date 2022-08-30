d56caa2272259660aa7ed3ec543366665c88a50a
etoxin <etoxin@users.noreply.github.com>
2022-08-30 17:32:18 +1000

    Basic boxes

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,15 +1,12 @@
+import * as THREE from "three";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useFrame, ThreeElements } from "@react-three/fiber";
 
-export function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef();
-  // Hold state for hovered and clicked events
+export function Box(props: ThreeElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-  // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
