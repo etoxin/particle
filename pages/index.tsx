f34d53f9ade69a00f57edbbe65e9c686d0a5363a
etoxin <etoxin@users.noreply.github.com>
2022-08-30 17:27:50 +1000

    Added prettier 🙄

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import {Box} from "../components/Box"
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Box } from "../components/Box";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-        </Canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
