d09e3d3c0448c2cc4bf645d78557d11f279b7981
etoxin <etoxin@users.noreply.github.com>
2022-09-02 17:07:47 +1000

    A few changes

--- components/Stage.tsx
+++ components/Stage.tsx
@@ -5,7 +5,7 @@ import { useControls } from "leva";
 
 export function Stage() {
   const props = useControls({
-    size: { value: 5, min: 1, max: 1000, step: 1 },
+    size: { value: 50, min: 1, max: 1000, step: 1 },
   });
   return (
     <Canvas>
